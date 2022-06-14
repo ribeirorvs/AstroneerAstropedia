54badccedbefc00d9184f2fa2b14865dd79660a0
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2022-06-14 15:45:16 -0300

    Create Russian lanaguage translate structure
    - Create the Russian language file
    - Include in the translate index the russian option
    - Set the main lanaguage to russian

--- astropedia/src/libs/localization/index.js
+++ astropedia/src/libs/localization/index.js
@@ -2,14 +2,16 @@ import i18n from 'i18n-js';
 import enUS from './en-US';
 import ptBR from './pt-BR';
 import plpl from './pl';
+import ruru from './ru';
 
 i18n.translations = {
     en: enUS,
     pt: ptBR,
-    pl: plpl
+    pl: plpl,
+    ru: ruru
 }
 
-i18n.locale = 'pl';
+i18n.locale = 'ru';
 
 //console.log(i18n.locale);
 
