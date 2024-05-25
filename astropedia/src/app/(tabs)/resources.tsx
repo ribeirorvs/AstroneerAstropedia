b5ce6c37e37976bb8090729bd8aa50ac987c6ccc
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2024-05-25 17:42:43 -0300

    Create the resources page

--- astropedia/src/app/(tabs)/resources.tsx
+++ astropedia/src/app/(tabs)/resources.tsx
@@ -1,5 +1,30 @@
-import { Text } from "react-native"
+import { ResourceList } from '@/assets/resources';
+import { ComponentButton } from '@/components/componentButton';
+import { Title } from '@/components/title';
+import { translate } from '@/libs/localization';
+import { layoutStyle } from '@/styles/layoutStyles';
+import { View, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Resources(){
-    return <Text>Resources</Text>
+export default function Resources() {
+    return (
+        <SafeAreaView style={layoutStyle.container}>
+            <View style={layoutStyle.content} >
+
+                <Title title={translate('resourcesTitle')} />
+                <FlatList
+                    data={ResourceList}
+                    keyExtractor={(item) => String(item.id)}
+                    renderItem={({ item }) => (
+                        <ComponentButton
+                            title={translate(item.title)}
+                            icon={item.icon}
+                            link={item.link}
+                        />
+                    )}
+                    style={layoutStyle.flatList}
+                />
+            </View>
+        </SafeAreaView>
+    )
 }
