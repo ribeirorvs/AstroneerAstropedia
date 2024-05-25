f49465430568f7b17f8f7acad530aee291e7b629
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2024-05-25 17:49:58 -0300

    Create the printers page

--- astropedia/src/app/(tabs)/printers.tsx
+++ astropedia/src/app/(tabs)/printers.tsx
@@ -1,5 +1,30 @@
-import { Text } from "react-native"
+import { PrintersList } from '@/assets/printers';
+import { ComponentButton } from '@/components/componentButton';
+import { Title } from '@/components/title';
+import { translate } from '@/libs/localization';
+import { layoutStyle } from '@/styles/layoutStyles';
+import React from 'react';
+import { View, FlatList, SafeAreaView } from 'react-native';
 
-export default function Printers(){
-    return <Text>Printers</Text>
+export default function Printers() {
+    return (
+        <SafeAreaView style={layoutStyle.container}>
+            <View style={layoutStyle.content} >
+
+                <Title title={translate('printers')} />
+                <FlatList
+                    data={PrintersList}
+                    keyExtractor={(item) => String(item.id)}
+                    renderItem={({ item }) => (
+                        <ComponentButton
+                            title={translate(item.title)}
+                            icon={item.icon}
+                            link={item.link}
+                        />
+                    )}
+                    style={layoutStyle.flatList}
+                />
+            </View>
+        </SafeAreaView>
+    )
 }
