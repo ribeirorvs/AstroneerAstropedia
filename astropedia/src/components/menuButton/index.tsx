a90dc9e70db26cdcdb91314484c8b97bba087005
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2024-06-06 23:21:56 -0300

    Create the menu button

---
+++ astropedia/src/components/menuButton/index.tsx
@@ -0,0 +1,21 @@
+import { Pressable } from "react-native";
+import {MaterialIcons} from "@expo/vector-icons";
+import {useNavigation} from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
+import colors from "@/styles/colors";
+
+export function MenuButton(){
+
+    const navigation = useNavigation();
+    const toggleMenu = () => navigation.dispatch(DrawerActions.toggleDrawer())
+
+    return (
+        <Pressable onPress={toggleMenu}>
+            <MaterialIcons
+                name="menu"
+                size={25}
+                color={colors.white}
+            />
+        </Pressable>
+    )
+}
